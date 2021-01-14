d525c3a6347ca7f2418394aa461cf20c834b7072
27_su <32132152+27su@users.noreply.github.com>
2021-01-14 14:30:09 +0900

    백트래킹

---
+++ N과M(11).cpp
@@ -0,0 +1,38 @@
+//백준 : https://www.acmicpc.net/problem/15665
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> result;
+int N,M;
+vector <int> arr;
+
+void getResult(int index){
+    if(index==M){
+        for(int i=0; i<M; i++) printf("%d ", result[i]);
+        printf("\n");
+        return;
+    }
+    
+    int before = -1;
+    for(int i=0; i<(int)arr.size(); i++){
+        if(arr[i] == before) continue;
+        result.push_back(arr[i]);
+        getResult(index+1);
+        result.pop_back();
+        before = arr[i];
+    }
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++){
+        int temp;
+        scanf("%d", &temp);
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end());
+    getResult(0);
+    return 0;
+}
