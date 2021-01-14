4847a2f4d18edcc95cbc4425b99e6ad4eba239d4
27_su <32132152+27su@users.noreply.github.com>
2021-01-14 14:27:28 +0900

    백트래킹

---
+++ N과M(10).cpp
@@ -0,0 +1,42 @@
+//백준 : https://www.acmicpc.net/problem/15664
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> result;
+int N,M;
+vector <bool> check;
+vector <int> arr;
+
+void getResult(int index, int start){
+    if(index==M){
+        for(int i=0; i<M; i++) printf("%d ", result[i]);
+        printf("\n");
+        return;
+    }
+    
+    int before = -1;
+    for(int i=start; i<(int)arr.size(); i++){
+        if(check[i] || arr[i] == before) continue;
+        check[i] = true;
+        result.push_back(arr[i]);
+        getResult(index+1, i+1);
+        result.pop_back();
+        check[i] = false;
+        before = arr[i];
+    }
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++){
+        int temp;
+        scanf("%d", &temp);
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end());
+    check.assign(N+1,0);
+    getResult(0,0);
+    return 0;
+}
